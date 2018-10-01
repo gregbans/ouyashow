64ee3bfb1f9bdde34e287998f98386561d81f2c0
bans <gregory.bannier@gmail.com>
2018-10-01 15:15:43 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
+import { FormulaireComponent } from './formulaire/formulaire.component';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+
+const appRoutes: Routes =[
+  { path: 'acceuil', component: AcceuilComponent },
+  { path: 'formulaire',      component: FormulaireComponent },
+  { path : '', component: AcceuilComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormulaireComponent,
+    AcceuilComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UiModule,
+    RouterModule.forRoot(appRoutes,{ enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
