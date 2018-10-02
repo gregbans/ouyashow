171a678d52d807ec877995e5042152be9ec61696
Nicolas LAURIO <nicolas.laurio@southside-interactive.com>
2018-10-02 09:32:11 +0200

    Modif Nico

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-
+//Ajouté par nico
+import { RouterModule, Routes } from '@angular/router';
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    //ajout nico
+    RouterModule
+
   ],
   declarations: [LayoutComponent, HeaderComponent, FooterComponent],
   exports: [LayoutComponent]
